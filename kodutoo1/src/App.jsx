daacbf9c5f011c6611a51cb81fbb66a261a3a8fa
KT147 <kevin.tammm@gmail.com>
2024-12-18 13:21:06 +0200

    kodutoo1

---
+++ kodutoo1/src/App.jsx
@@ -0,0 +1,71 @@
+// import { useState } from 'react'
+// import reactLogo from './assets/react.svg'
+// import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+//   const [count, setCount] = useState(0)
+
+
+  return (
+    <>
+      <button className='nupp'>Nupp</button>
+      <br></br>
+
+      <div className='sinine'><i>Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugiat, cum non ea eos officia sapiente distinctio labore ipsa ut architecto magnam. Quaerat ad dignissimos velit explicabo nostrum dolores repellendus praesentium!</i></div>
+
+      <br></br>
+
+      <div className='roheline'><b>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorum earum nam delectus ad obcaecati soluta totam quas repellendus porro quasi ex quibusdam, ipsa fuga hic laboriosam. Dicta esse minima in.</b></div>
+
+      <img src='https://swiftorg.co.uk/wp-content/uploads/2022/09/Snooker-7.jpeg' className='pilt'></img>
+
+    <h2>Tabel</h2>
+
+      <table className='tabel'>
+        <tr>
+          <th>Ettevõte</th>
+          <th>Kontakt</th>
+          <th>Riik</th>
+        </tr>
+        <tr>
+          <td>Alfreds Futterkiste</td>
+          <td>Maria Anders</td>
+          <td>Saksamaa</td>
+        </tr>
+        <tr>
+          <td>Centro comercial Moctezuma</td>
+          <td>Francisco Chang</td>
+          <td>Mehhiko</td>
+        </tr>
+        <tr>
+          <td>Ernst Handel</td>
+          <td>Roland Mendel</td>
+          <td>Austria</td>
+        </tr>
+        <tr>
+          <td>Island Trading</td>
+          <td>Helen Bennett</td>
+          <td>Suurbritannia</td>
+        </tr>
+         <tr>
+          <td>Laughing Bacchus Winecellars</td>
+          <td>Yoshi Tannamuri</td>
+          <td>Kanada</td>
+        </tr>
+        <tr>
+          <td>Magazzini Alimentari Riuniti</td>
+          <td>Giovanni Rovelli</td>
+          <td>Itaalia</td>
+         </tr>
+    </table>
+
+    <br></br>
+    
+    <iframe width="560" height="315" src="https://www.youtube.com/embed/7amiTeHjAwM?si=7ile0Cj0y_ATdXe_" title="YouTube video player" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
+
+    </>
+  )
+}
+
+export default App
